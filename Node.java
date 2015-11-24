9f82e559019d3d8b17b9bd3ad27cefc66c43e771
Jovan Zivanovic <zivanovicjovanprivatemail@gmail.com>
2015-11-24 10:34:01 +0100

    Neu geschrieben

--- Node.java
+++ Node.java
@@ -1,6 +1,31 @@
 
-public abstract class Node {
+public class Node {
+	private Object element;
+    private Node next;
 
-    public Node next;
+    public Node(Object element, Node next){
+    	this.element = element;
+    	this.next = next;
+    }
 
+    public Node(Object element){
+    	this.element = element;
+    	this.next = null;
+    }
+
+    public Object getElement(){
+    	return this.element;
+    }
+
+    public Node getNext(){
+    	return this.next;
+    }
+
+    public boolean hasNext(){
+    	return this.next != null;
+    }
+
+    public void setNext(Node next){
+    	this.next = next;
+    }
 }
